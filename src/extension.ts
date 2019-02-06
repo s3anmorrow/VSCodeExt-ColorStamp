8fde9e659fec00bef9d3d66742e4e4707cb273da
Sean Morrow <sean.morrow@nscc.ca>
2019-02-06 08:41:50 -0400

    titleBar changing to set color

--- src/extension.ts
+++ src/extension.ts
@@ -1,23 +1,55 @@
+// color theme : overflow (rainglow)
+
+
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 
+
+async function colorTitleBar() {
+
+	// THIS IS WORKING FOR THEME CHANGE
+	//let config = vscode.workspace.getConfiguration();
+	//await config.update('workbench.colorTheme', 'Allure Light (rainglow)', true);
+	// ---------------------------------
+
+
+	//let config = vscode.workspace.getConfiguration("workbench").get("colorCustomizations");
+	//let config = vscode.workspace.getConfiguration("workbench.colorCustomizations");
+	//	.get("colorCustomizations.titleBar.activeBackground");
+	//await config.update('titleBar.activeBackground', '#FF0000', undefined);
+
+	let config = vscode.workspace.getConfiguration();
+	let value = {
+		"titleBar.activeBackground":"#00AA00",
+		"activityBar.background": "#00AA00"
+	};
+	// undefined so it only updates the workspace configurations and not globally
+	// updates the .vscode/settings.json file of project folder
+	await config.update("workbench.colorCustomizations", value, undefined);
+
+	
+
+
+	
+}
+
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
-export function activate(context: vscode.ExtensionContext) {
-
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "helloWorld" is now active!');
-
+export async function activate(context: vscode.ExtensionContext) {
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
 	// The commandId parameter must match the command field in package.json
 	let disposable = vscode.commands.registerCommand('extension.helloWorld', () => {
+
+		// check if VS Code has project folder open - if not this extension does nothing :(
+		
+
+
 		// The code you place here will be executed every time your command is executed
 
 		
-
+		colorTitleBar();
 
 
 		// Display a message box to the user
