db3b4baa7aed98c02930e25e80eee6d5e2535d97
Sean Morrow <sean.morrow@nscc.ca>
2019-02-06 09:26:34 -0400

    Input color hex code implemented with error checking

--- src/extension.ts
+++ src/extension.ts
@@ -1,10 +1,15 @@
 // color theme : overflow (rainglow)
 
+// MANUALLY APPROACH
+// https://medium.com/@camdenb/colorful-vscode-titlebars-for-better-productivity-b05a619defed
+// THEME COLOR CHANGING
+// https://code.visualstudio.com/api/references/theme-color
+// SAMPLES
+// https://marketplace.visualstudio.com/items?itemName=RolandGreim.sample-ext
+
 
 // The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
-
 
 async function colorTitleBar() {
 
@@ -19,16 +24,51 @@ async function colorTitleBar() {
 	//	.get("colorCustomizations.titleBar.activeBackground");
 	//await config.update('titleBar.activeBackground', '#FF0000', undefined);
 
+	// regular expression to validate hex color user input
+	let regex:RegExp = new RegExp("^#([A-Fa-f0-9]{6}|[A-Fa-f0-9]{3})$");
+
+	// getting color hexcode input from user
+	let options: vscode.InputBoxOptions = {
+		password: false,
+		placeHolder: "#FF0000",
+		prompt: "Enter the color in hexcode:",
+		validateInput: (text: string) => {
+			if (!text.match(regex)) {
+				return "Not a valid Color Hex Code yet :(";
+			} else {
+				return null;
+			}
+		}
+	};
+	vscode.window.showInputBox(options).then((enteredColor: any) => {
+		vscode.window.showInformationMessage('You entered ' + enteredColor);
+
+
+		// get reference to workspace configuration and set titleBar color
+		let config:vscode.WorkspaceConfiguration = vscode.workspace.getConfiguration();
+		let value:Object = {
+			"titleBar.activeBackground": enteredColor,
+			"activityBar.background": enteredColor
+		};
+		// undefined so it only updates the workspace configurations and not globally
+		// updates the .vscode/settings.json file of project folder
+		//await config.update("workbench.colorCustomizations", value, undefined);
+		config.update("workbench.colorCustomizations", value, undefined);
+	});
+
+
+
+	/*
 	// get reference to workspace configuration and set titleBar color
-	let config = vscode.workspace.getConfiguration();
-	let value = {
+	let config:vscode.WorkspaceConfiguration = vscode.workspace.getConfiguration();
+	let value:Object = {
 		"titleBar.activeBackground":"#00AA00",
 		"activityBar.background": "#00AA00"
 	};
 	// undefined so it only updates the workspace configurations and not globally
 	// updates the .vscode/settings.json file of project folder
 	await config.update("workbench.colorCustomizations", value, undefined);
-
+	*/
 	
 
 
@@ -44,7 +84,6 @@ export async function activate(context: vscode.ExtensionContext) {
 	let disposable = vscode.commands.registerCommand('extension.helloWorld', () => {
 
 		// check if VS Code has project folder open - if not this extension does nothing :(
-		// window.showErrorMessage
 		if (vscode.workspace.workspaceFolders === undefined) {
 			vscode.window.showErrorMessage("colorMe Error : No project folder (workspace) opened");
 		} else {
